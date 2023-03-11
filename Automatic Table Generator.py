0f51e4762b6f8f5264b3dc043cf7ff7c9545d4f0
Muntaha25 <muntaha1312@gmail.com>
2023-03-11 11:05:00 +0500

    initial commit

---
+++ Automatic Table Generator.py
@@ -0,0 +1,5 @@
+i = int (input())
+for l in range (11):
+    num = l * i
+    p = f'{i} x {l} = {num}'
+    print(p)
---
+++ Variance and Std Deviation Calculator.py
@@ -0,0 +1,54 @@
+#def my_sum(num1, num2, *args):
+ #   return num1 + num2 + sum(args)
+#my_sum(1, 2, 3)
+
+
+#x=int(input())
+#while x != 0:
+    #y=int(input())
+    #z= sum(x,y)
+    #print(z)
+
+#from functools import reduce
+
+#def mysum(*nums):
+ #   return reduce(lambda x, y: x+y, nums)
+
+#a, b, c = 1, 2, 3
+
+#res = mysum(a,b,c)
+
+
+
+import numpy as np
+i = []
+add = i.add(i)
+
+print (add)
+
+
+#a = int(input())
+#ask = "yes"
+#while (ask == "yes"):
+    #b = int(input("what is the number you want to add?: "))
+    #a = sum([a,b])
+    #ask = input("do you want to add another number?: ")
+
+#print (a)
+
+
+
+ # print(i)
+#while 
+#for i in range(6):
+ #x = (sum(i)/n)
+#v = (i - x)**2/n
+#print('Calculated Variance =' , s)
+
+
+
+
+
+#sd = (i - x)/n*n
+#p2 = sum(sd)
+#print('Calculated Standard Deviation =' ,p2)
